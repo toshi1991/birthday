80d301f8286da7463ed6d0fb4d3da74d98939216
kbc-13-31-PC\kbc-13-31 <toshihito.iha@gmail.com>
2016-10-06 13:02:14 +0900

    update er-figure (add admin_flg to Users), Change root page to Messages/index, update css

--- config/routes.php
+++ config/routes.php
@@ -49,7 +49,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Users', 'action' => 'top']);
+    $routes->connect('/', ['controller' => 'Messages', 'action' => 'index']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
